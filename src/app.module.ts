014cf4ac2001829e7a734f6c6b600a959e3fd045
donghquinn <ehdgus1524@gmail.com>
2024-01-04 02:26:46 +0900

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@
 import { HackerModule } from '@modules/hacker.module';
 import { HadaModule } from '@modules/hada.module';
 import { MachineLearningNewsModule } from '@modules/machine.module';
-import { PrismaModule } from '@modules/prisma.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { globalMiddleware } from 'middlewares/header.middleware';
 
 @Module({
-  imports: [HackerModule, PrismaModule, MachineLearningNewsModule, HadaModule],
+  imports: [HackerModule, MachineLearningNewsModule, HadaModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
