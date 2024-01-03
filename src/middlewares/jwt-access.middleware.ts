93b228768a8f860083d92f9a82f8c01e716fb645
donghquinn <ehdgus1524@gmail.com>
2024-01-04 02:18:53 +0900

    wip: format

--- src/middlewares/jwt-access.middleware.ts
+++ src/middlewares/jwt-access.middleware.ts
@@ -1,21 +1,19 @@
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { ClientLoginRequest } from "types/client.type";
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ClientLoginRequest } from 'types/client.type';
 
-export class JwtAccessStrategy extends PassportStrategy( Strategy, "access" ) {
-    constructor ()
-    {
-        super( {
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: process.env.ACCESS_TOKEN_SECRET_KEY,
-        } );
-    }
+export class JwtAccessStrategy extends PassportStrategy(Strategy, 'access') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET_KEY,
+    });
+  }
 
-    static validate ( payload: ClientLoginRequest )
-    {
-        return {
-            email: payload.email,
-            password: payload.password
-        }
-    }
+  static validate(payload: ClientLoginRequest) {
+    return {
+      email: payload.email,
+      password: payload.password,
+    };
+  }
 }
