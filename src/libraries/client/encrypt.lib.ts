738d7dc95bbabda3b66719b2de0130087ea5fe26
donghquinn <ehdgus1524@gmail.com>
2024-02-23 17:36:26 +0900

    wip: fixing account login

--- src/libraries/client/encrypt.lib.ts
+++ src/libraries/client/encrypt.lib.ts
@@ -4,14 +4,14 @@ import { createCipheriv, randomBytes } from 'crypto';
  * @param certKey 고객 인증키. 임의의 16파이트 키
  * @returns hashToken - token 필드에 들어갈 값, uuid - 고객 uuid
  */
-export const cryptPassword = (password: string) => {
+export const cryptData = (data: string) => {
   const secretKey = process.env.SECRET_KEY!;
   const iv = randomBytes(16); // Initialization vector
   const cipher = createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
 
-  const encrypted = cipher.update(password);
+  const encrypted = cipher.update(data);
 
   const encryptedString = Buffer.concat([encrypted, cipher.final()]).toString('hex');
 
-  return { passwordToken: iv.toString('hex'), encodedPassword: encryptedString };
+  return { encodedToken: iv.toString('hex'), encodedData: encryptedString };
 };
