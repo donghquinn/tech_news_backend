14c5942a5a07770323b8aa8e899ce26c38e245e7
donghquinn <ehdgus1524@gmail.com>
2024-03-03 22:27:38 +0900

    wip: main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from 'app.module';
@@ -20,15 +21,15 @@ export const bootstrap = async () => {
   const accountManager = new AccountManager();
   await accountManager.start();
   
-  // const corsOptions: CorsOptions = {
-  //   origin: 'https://scrape.donghyuns.com',
-  //   allowedHeaders: ['GET', 'POST', 'Content-Type', 'key'],
-  //   optionsSuccessStatus: 204,
-  //   preflightContinue: false,
-  // };
+  const corsOptions: CorsOptions = {
+    origin: 'https://scrape.donghyuns.com',
+    allowedHeaders: ['GET', 'POST', 'Content-Type', 'key'],
+    optionsSuccessStatus: 204,
+    preflightContinue: false,
+  };
 
   app.use(helmet());
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.enableVersioning();
   app.useBodyParser('json');
   app.enableShutdownHooks();
