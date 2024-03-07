e13f475a424cc00e522ea57c155d253612fa049b
donghquinn <ehdgus1524@gmail.com>
2024-03-07 23:48:35 +0900

    create server code

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,5 @@
-import { Logger } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from 'app.module';
-import helmet from 'helmet';
-import { AccountManager } from 'providers/auth/account-manager.pvd';
-import { shutdown } from 'utils/shutdown.utils';
+import { bootstrap } from "server";
 
 process.env.TZ = 'Asia/Seoul';
-
-export const bootstrap = async () => {
-  const date = new Date().toLocaleTimeString();
-
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['log', 'debug', 'warn', 'error'],
-  });
-
-  const port = Number(process.env.APP_PORT);
-
-  const accountManager = new AccountManager();
-  await accountManager.start();
-
-  const corsOptions: CorsOptions = {
-    origin: 'https://scrape.donghyuns.com',
-    allowedHeaders: ['GET', 'POST', 'Content-Type', 'key'],
-    optionsSuccessStatus: 204,
-    preflightContinue: false,
-  };
-
-  app.use(helmet());
-  app.enableCors(corsOptions);
-  app.enableVersioning();
-  app.useBodyParser('json');
-  app.enableShutdownHooks();
-
-  await app.listen(port, '0.0.0.0', () => {
-    const message = `Listening On ${port}`;
-    const wrapper = '@'.repeat(message.length);
-
-    Logger.log(wrapper);
-    Logger.log(`Scrape Manager Start: ${date}`);
-    Logger.log(message);
-    Logger.log(wrapper);
-
-    process.send?.('ready');
-  });
-
-  process.on('SIGTERM', () => shutdown(app));
-};
 
 await bootstrap();
